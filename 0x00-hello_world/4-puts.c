a881a26a4c9501a9364a46d8de05f18d6da70e90
Steve <ustinsteve@gmail.com>
2023-06-15 12:26:31 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,8 @@
-#!/bin/bash
+#include <stdio.h>
+int main(void)
+{
+	char Str[] = "Programming is like building a multilingual puzzle\n";
+
+	puts(Str); //Printing the strings to standard out
+	return(0);
+}
