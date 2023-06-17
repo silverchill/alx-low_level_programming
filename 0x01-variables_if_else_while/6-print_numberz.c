2f05006526e76e54aaa1ab433bf38149951e4e2d
Steve <ustinsteve@gmail.com>
2023-06-17 10:40:52 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - The get all single nos of base 10 using putchar
+ *Return: 0 (Successful)
+ */
+int main(void)
+{
+	char p = 0;
+
+	for (p = 0; p < 10; p++)
+	{
+		putchar(p);
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
