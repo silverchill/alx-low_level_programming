c2f6693ddfcf43aab2ce0d3b867438888b44697e
Steve <ustinsteve@gmail.com>
2023-06-17 09:18:27 -0700

    4-print_alphabt.c added

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main - To print the lower case alphabets
+ *Return: 0 (Successful)
+ */
+int main(void)
+{
+	int s = 97;
+
+	while (s <= 122)
+	{
+		if (s == 101 || s == 113)
+		{
+			s++;
+			continue;
+		}
+		putchar(s);
+		s++;
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
