5b1b4ad1ab85c0320f8eca326af2e941f8325daf
Steve <ustinsteve@gmail.com>
2023-06-18 07:15:34 -0700

    5-printf.c added

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ *main - To print exact grammer using printf
+ *Return: 0 (Successful)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
