4f8377021a454304c3367103fad3d4ce7948e0dc
Steve <ustinsteve@gmail.com>
2023-06-18 10:13:20 -0700

    101-quote.c added

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,14 @@
-#!/bin/bash
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *main - To print to a standard error
+ *Return: 0 (Successful)
+ */
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fwrite(str, sizeof(char), sizeof(str) - 1, stdout);
+	
+	return (0);
+}
