7e081a771b9538db2d32693b03fc80b95670f831
Steve <ustinsteve@gmail.com>
2023-06-18 08:58:49 -0700

    6-size.c added

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,14 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ *main - To get the size of various types on the computer its compiled n run on
+ *Return: 0 (successful)
+ */
+int main(void)
+{
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
+	return (0);
+}
