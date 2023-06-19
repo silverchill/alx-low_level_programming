66e458f64f75eb6bfd155bc0e8d074dbe5a3ccfb
Steve <ustinsteve@gmail.com>
2023-06-19 09:03:18 -0700

    101-quote.c added

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 /**
  *main - To print a standard error
- *Return: 0 (Successful)
+ *Return: 1 (Successful)
  */
 int main(void)
 {
 	system("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'");
-	return (0);
+	return (1);
 }
