962f8e2d17295da932821e9677b387364625453e
Steve <ustinsteve@gmail.com>
2023-06-20 14:41:04 -0700

    5-sign added

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,18 +8,18 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(0);
+		_putchar(48);
 		return (1);
 	}
-	else if (n == 0)
+	else if (n < 0)
 	{
-		_putchar(0);
-		return (0);
+		_putchar(48);
+		return (-1);
 	}
 	else
 	{
-		_putchar(0);
-		return (-1);
+		_putchar(48);
+		return (0);
 	}
 	_putchar('\n');
 }
