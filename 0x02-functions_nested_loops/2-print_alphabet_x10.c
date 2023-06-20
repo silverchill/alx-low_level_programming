fb15b17165da3bbc90cef6c2bf1ac6198921a6aa
Steve <ustinsteve@gmail.com>
2023-06-20 11:44:44 -0700

    2-print_alphabet_x10.c added

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,2 +1,18 @@
 #include "main.h"
-/
+/**
+ *print_alphabet_x10 - To print 10 times the alphabet, in lowercase
+ *Return: 0 (Successful)
+ */
+void print_alphabet_x10(void)
+{
+	char p, q;
+
+	for (q = 1; p <= 10; p++)
+	{
+		for (q = 97; q < 123; q++)
+		{
+			_putchar(q);
+		}
+		_putchar('\n');
+	}
+}
