0100c0a9adbbad552157ef34882ae4e861e758e9
Steve <ustinsteve@gmail.com>
2023-06-20 13:54:51 -0700

    2-print_alphabet_x10.c added

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *print_sign - To print the sign of a number
+ *@n: Is a character
+ *Return: 1 is greater than 0, 0 is zero and -1 is less than 0
+ */
+int print_sign(int n)
+{
+	for (n > 0)
+	{
+		_putchar(48);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(48);
+		return (-1);
+	}
+	_putchar('\n');
+}
