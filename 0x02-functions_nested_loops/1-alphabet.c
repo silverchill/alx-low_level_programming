9560e6dbc2b48bc7b75e3059ec409f3e0a483b61
Steve <ustinsteve@gmail.com>
2023-06-20 09:01:11 -0700

    1-alphabet.c added

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
- *print_alphabet - check the code
+ *main - Print lower letter
  *Return: Always 0.
  */
-void print_alphabet(void)
-	int j;
-	
+int main(void)
+{
+	char j;
+
 	for (j = 97; j < 123; j++)
 	{
 		_putchar(j);
 	}
 	_putchar('\n');
 	return (0);
-
+}
