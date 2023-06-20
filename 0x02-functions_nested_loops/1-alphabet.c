d40e626dca058dd26716fcb7e4bff5ca59453791
Steve <ustinsteve@gmail.com>
2023-06-20 08:19:06 -0700

    1-alphabet.c added

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *main - check the code
+ *Return: Always 0.
+ */
+int main(void)
+	int j;
+	
+	for (j = 97; j < 123; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+	return (0);
+
