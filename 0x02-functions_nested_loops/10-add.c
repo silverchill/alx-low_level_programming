2f3884b489f185849cac70c6eff4612e673d4d2d
Steve <ustinsteve@gmail.com>
2023-06-21 09:10:16 -0700

    10-add.c added

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *add - To print adds two integers and returns the result
+ *@b: Is an interger
+ *@d: Is an interger
+ *Return: sum of b and d
+ */
+int add(int b, int d)
+{
+	int A;
+
+	A = (b + d);
+	return (A);
+}
