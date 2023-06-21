69dc46837df9cf6664e73b53cdc3483d8cca4803
Steve <ustinsteve@gmail.com>
2023-06-21 06:38:56 -0700

    9-times_table.c added

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,5 +6,32 @@
  */
 void times_table(void)
 {
+	int w, y, z;
 
+	for (w = 0; w <= 9; w++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			z = w * y;
+			if (w == 0)
+			{
+				_putchar(z + 0);
+			}
+			if (z <= 9 && w != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(w + 0);
+			}
+			else if (w >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((w / 10) + 0);
+				_putchar((w % 10) + 0);
+			}
+		}
+	}
+	_putchar('\n');
 }
