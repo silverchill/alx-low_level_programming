43f8feb006926fe4b8b14ced85b195d7a19be51d
Steve <ustinsteve@gmail.com>
2023-06-22 07:52:45 -0700

    1-isdigit added

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_numbers - To prints the numbers, from 0 to 9
+ *Return: 0 Successful
+ */
+void print_numbers(void)
+{
+	int g;
+
+	for (g = 48; g < 58; g++)
+	{
+		_putchar('g');
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *print_most_numbers - To prints the numbers, from 0 to 9
+ *Return: 0
+ */
+void print_most_numbers(void)
+{
+	int v;
+
+	for (v = 48; v <= 57; v++)
+	{
+		if (v == 50 || v == 52)
+		{
+			v++;
+			continue;
+		}
+		_putchar(v);
+	}
+	_putchar('\n');
+}
