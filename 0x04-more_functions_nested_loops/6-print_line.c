54bc49bb6288bbe456ce6e1fc5ef708f4e3f19be
Steve <ustinsteve@gmail.com>
2023-06-23 03:12:12 -0700

    6-lines.c added

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *print_line - To draws a straight line in the terminal
+ *@n: no of times to be printed
+ *Return: 0
+ */
+void print_line(int n)
+{
+	int m = 1;
+
+	if (m <= n)
+	{
+		_putchar('_');
+		m++;
+	}
+	else
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+	}
+	
+}
