d0159e724428108d136d88990185c06b9dda4394
Steve <ustinsteve@gmail.com>
2023-06-23 04:50:47 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,17 +7,18 @@
  */
 void print_line(int n)
 {
-	if (n > 1)
-	{
-		_putchar('_');
-		_putchar('\n');
-	}
-	else
-	{
-		if (n <= 0)
-		{
-			_putchar('\n');
-		}
-	}
-	
+       if (n <= 0)
+       {
+	       _putchar('\n');
+       }
+       else
+       {
+	       int y;
+
+	       for (y = 1; y <= n; y++)
+	       {
+		       _putchar('_');
+	       }
+	       _putchar('\n');
+       }
 }
