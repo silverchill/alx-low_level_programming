4734e87f5815fc8d5f2f00f053b0f330188d760b
Steve <ustinsteve@gmail.com>
2023-06-26 09:40:39 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *reset_to_98 - To print a pointer to an int as parameter and updates the value
+ *@n: is a dereference
+ *Return: 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Description of 0x05. C - Pointers, arrays and strings
