98021b7de2f57efc0a8a9fad11f7fae0b85b4556
Steve <ustinsteve@gmail.com>
2023-07-13 11:57:39 -0700

    101-mul.c

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * main - To print multiplies two positive numbers
+ * @argc: no of arguments
+ * @argv: array of argument
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	char *s1, *s2;
+	int length1, length2, length, carry, num1, numb2, *res, h = 0;
+
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !
+}
