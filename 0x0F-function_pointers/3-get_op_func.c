974d93d717ca474688f243d3e4edf78750472579
Steve <ustinsteve@gmail.com>
2023-07-19 11:43:24 -0700

    solution to task 3

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ * get_op_func - To print the fxn selects the correct function to perform
+ * the operation asked by the user
+ *
+ * @s: Is the operator passed as argument to the program
+ *
+ * Return: Pointer to the fxn
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	for (i = 0; (ops[i].op); i++)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+		return (ops[i].f);
+	}
+	return (NULL);
+}
+
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - To return the sum of a and b
+ * @a: first interger
+ * @b: Second interger
+ * Return: The sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - To return the difference of a and b
+ * @a: first interger
+ * @b: second interger
+ * Return: The difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - To return the product of a and b
+ * @a: first interger
+ * @b: second interger
+ * Return: The product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - To return the result of the division of a by b
+ * @a: first interger
+ * @b: second interger
+ * Return: The quotient of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - To return the remainder of the division of a by b
+ * @a: first interger
+ * @b: second interger
+ * Return: The modulus of a and b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
