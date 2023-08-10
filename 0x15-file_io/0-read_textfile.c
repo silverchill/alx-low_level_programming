7597ca26cb3783d7ec7b4e0a62fc191cc1d6c532
Steve <ustinsteve@gmail.com>
2023-08-10 20:19:36 +0100

    Update to 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,10 +5,10 @@
 /**
  * read_textfile - To print fxn that reads a text file and
  * printsit to the POSIX standard output
- * 
+ *
  * @filename: Is the pointer Variable
  * @letters: Is the number of letters it should read and print
- * 
+ *
  * Return: the actual number of letters it could read and print, if the file
  * can not be opened or read, return 0, if filename is NULL return 0, if write
  * fails or does not write the expected amount of bytes, return 0
@@ -27,7 +27,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	buffer = malloc(letters * sizeof(char));
-	
+
 	nRead = read(fd, buffer, letters);
 	if (nRead == -1)
 		return (0);
