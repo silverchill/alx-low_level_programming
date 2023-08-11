a0397f8ff3545a3dbeac0e85ccfa22eebef42ea1
Steve <ustinsteve@gmail.com>
2023-08-11 11:34:25 +0100

    Update to 1-create_file.c

--- 0x15-file_io/1-main.c
+++ 0x15-file_io/1-main.c
@@ -3,10 +3,11 @@
 #include "main.h"
 
 /**
- *  * main - check the code
- *   *
- *    * Return: Always 0.
- *     */
+ * main - check the code
+ * @ac: no of arguments
+ * @av: double pointer of array of arguments
+ * Return: Always 0.
+ */
 int main(int ac, char **av)
 {
 	int res;
